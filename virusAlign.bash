5f4fab844331bde8007ccd41671b9e2144780a9d
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2015-12-28 07:21:51 -0500

    Virus alignment

--- virusAlign.bash
+++ virusAlign.bash
@@ -7,32 +7,36 @@ for thisDir in data/*;do
 	echo $thisDir
 	virusFile=$thisDir/*.fasta
 	echo $virusFile
-	virusDir=work/$(basename $virusFile)
-	virusDir=${virusDit%.fasta}
-	echo $virusDir
-	#if [ ! -e 
-	#~/installs/star/bin/Linux_x86_64/STAR   --runMode genomeGenerate   --runThreadN 24   --genomeDir ./ --sjdbGTFfile knownGene.gtf  --genomeFastaFiles *.fa
-
+	virusDir=work/virusGenomes/$(basename $virusFile)
+	virusDir=${virusDir%.fasta}
+	if [ ! -e $virusDir ];then
+		echo "Working on $virusDir"
+		mkdir -p $virusDir
+		#map HIV89.6 junctions virus? try just mapping total first
+		#~/installs/star/bin/Linux_x86_64/STAR   --runMode genomeGenerate   --runThreadN 24   --genomeDir ./ --sjdbGTFfile knownGene.gtf  --genomeFastaFiles *.fa --genomeSAindexNbases 4
+		~/installs/star/bin/Linux_x86_64/STAR   --runMode genomeGenerate   --runThreadN 24   --genomeDir $virusDir --genomeFastaFiles $virusFile --genomeSAindexNbases 4
+	else
+		echo "$virusDir genome already built"
+	fi
 	for ii in $thisDir/*.fastq.gz;do
-		continue
 		echo $ii
 		base=$(basename $ii)
 		outFile=work/align/${base%_trim.fastq.gz}_virus
 		finalFile=$outFile.bam
 		if [ ! -f "$finalFile" ];then
 			date
-			./removeShort.py $ii >work/unzipped.fastq
+			./removeShort.py $ii >work/unzipped_virus.fastq
 			echo "Unzipped"
 			date
 			echo "Aligning"
-			~/installs/star/bin/Linux_x86_64/STAR --genomeDir ~/installs/star/index/  --runThreadN 24 --readFilesIn work/unzipped.fastq --outFileNamePrefix $outFile.
+			~/installs/star/bin/Linux_x86_64/STAR --genomeDir $virusDir  --runThreadN 4 --readFilesIn work/unzipped_virus.fastq --outFileNamePrefix $outFile.
 			echo "Sorting"
-			samtools view -bS "$outFile.Aligned.out.sam" > work/tmp.bam
-			samtools sort -m 5000000000 work/tmp.bam $outFile #use ~5G of RAM
+			samtools view -bS "$outFile.Aligned.out.sam" > work/tmp_virus.bam
+			samtools sort -m 5000000000 work/tmp_virus.bam $outFile #use ~5G of RAM
 			samtools index $finalFile
 			rm "$outFile.Aligned.out.sam"
-			rm work/unzipped.fastq
-			rm work/tmp.bam
+			rm work/unzipped_virus.fastq
+			rm work/tmp_virus.bam
 			echo "Done"
 		else
 			echo "Already processed. Skipping."
