c97c0497bcfec97ce113a4147eb0ca52142f31f9
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2015-12-30 21:53:44 -0500

    Script to run suffix tree

---
+++ findVirusSeqs.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e 
+
+
+for thisDir in data/*;do
+	echo $thisDir
+	virusFile=$thisDir/*.fasta
+	echo $virusFile
+	for ii in $thisDir/*.fastq.gz;do
+    echo $ii
+    base=$(basename $ii)
+    outFile=work/virusFind/${base%_trim.fastq.gz}
+		if [ ! -f "$outFile_match.fastq.gz" ];then
+			date
+      ~/installs/suffic $virusFile $ii -o $outFile -m 1 -l 12
+      date
+    else
+			echo "Already processed. Skipping."
+    fi
+done
