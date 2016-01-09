8cbc0b46d022f9db934ab0f03cbba14df3aac5b9
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-01-09 08:31:28 -0500

    Fixing caching

--- checkBases.R
+++ checkBases.R
@@ -1,11 +1,11 @@
 
 library(parallel)
 getBaseCounts<-function(bamFile){
-  message(bamFile)
-  outFile<-sub(bamFile,'bam$','baseCount')
+  outFile<-sub('bam$','baseCount.gz',bamFile)
+  message(bamFile,"->",outFile)
   cmd<-sprintf('~/.local/bin/countbases --strand %s|gzip>%s',bamFile,outFile)
   if(!file.exists(outFile))system(cmd)
-  else message("Using cached")
+  else message("Using cached ",outFile)
   output<-readLines(outFile)
   output[1]<-gsub('-','n',gsub('+','p',output[1]))
   con<-textConnection(output)
@@ -15,4 +15,4 @@ getBaseCounts<-function(bamFile){
 
 dataDir<-'work/virusAlign'
 bulkRnaFiles<-list.files(dataDir,'Total.*bam$')
-print(system.time(baseCounts<-mclapply(sprintf("%s/%s",dataDir,bulkRnaFiles),getBaseCounts,mc.cores=10)))
+if(!exists('baseCounts'))print(system.time(baseCounts<-mclapply(sprintf("%s/%s",dataDir,bulkRnaFiles),getBaseCounts,mc.cores=10)))
