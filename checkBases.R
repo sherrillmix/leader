3faac82eff169830bd0d14206629e7e777764dfe
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-01-09 08:03:21 -0500

    Cache checkbases

--- checkBases.R
+++ checkBases.R
@@ -1,9 +1,18 @@
+
+library(parallel)
 getBaseCounts<-function(bamFile){
-  cmd<-sprintf('countbases --strand %s',bamFile)
-  output<-system(cmd,intern=TRUE)
+  message(bamFile)
+  outFile<-sub(bamFile,'bam$','baseCount')
+  cmd<-sprintf('~/.local/bin/countbases --strand %s|gzip>%s',bamFile,outFile)
+  if(!file.exists(outFile))system(cmd)
+  else message("Using cached")
+  output<-readLines(outFile)
+  output[1]<-gsub('-','n',gsub('+','p',output[1]))
   con<-textConnection(output)
-  read.csv(con,stringsAsFactors=FALSE)
+  out<-read.csv(con,stringsAsFactors=FALSE)
+  return(out)
 }
 
-bulkRnaFiles<-list.files('work/virusAlign/','Total.*bam')
-baseCounts<-lapply(bulkRnaFiles,getBaseCounts)
+dataDir<-'work/virusAlign'
+bulkRnaFiles<-list.files(dataDir,'Total.*bam$')
+print(system.time(baseCounts<-mclapply(sprintf("%s/%s",dataDir,bulkRnaFiles),getBaseCounts,mc.cores=10)))
