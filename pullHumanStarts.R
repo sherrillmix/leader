cc8aa9b8bd99f900d210f48bc8c0227300e4688a
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-02-14 22:34:56 -0500

    Fix end/start for negative. Correct order of regions and bases. Pull all good and plot

--- pullHumanStarts.R
+++ pullHumanStarts.R
@@ -2,6 +2,7 @@ library(parallel)
 source("~/scripts/R/dna.R")
 
 getHuman<-function(bamFile,regions,strand=c('+','-'),sizeRange=26:30){
+  startEnd<-ifelse(all(strand=='-'),'end','start')
   counts<-lapply(regions,function(region){
     reg<-parseRegion(region)
     cmd<-sprintf('~/.local/bin/getstartends %s --region %s --maxGaps 3',bamFile,region)
@@ -9,7 +10,7 @@ getHuman<-function(bamFile,regions,strand=c('+','-'),sizeRange=26:30){
     inCon<-textConnection(output)
     out<-read.csv(inCon)
     close(inCon)
-    outTable<-unlist(table(out[out$end-out$start+1 %in% sizeRange & out$start>=reg$start&out$start<=reg$end & out$strand %in% strand,'start']))
+    outTable<-unlist(table(out[out$end-out$start+1 %in% sizeRange & out$start>=reg$start&out$start<=reg$end & out$strand %in% strand,startEnd]))
     outVector<-rep(0,reg$end-reg$start+1)
     names(outVector)<-reg$start:reg$end
     outVector[names(outTable)]<-outTable
@@ -70,37 +71,52 @@ coordToReg<-function(coord){
   #may need to adjust start
   sprintf('%s:%d-%d',seqnames(coord),start(coord),end(coord))
 }
-targetFiles<-sub('_virus','',allRnaFiles[sample=='HIV_CH0694'&!grepl('^Total',allRnaFiles)])
+targetFiles<-allRnaFiles[grepl('_HIV_',allRnaFiles)]#&!grepl('^Total',allRnaFiles)])
 dataDir<-'work/align/'
 
-windowWidth<-40
-startCounts<-mcmapply(function(five,cd,windowWidth=40,...){
+naFillRegs<-function(file,regs,naNs,...){
+  outs<-lapply(regs,function(reg,...)do.call(c,getHuman(file,reg,...)),...)
+  out<-mapply(function(dat,n){
+    if(n==0)return(dat)
+    if(n>0)return(c(dat,rep(NA,n)))
+    else return(c(rep(NA,-n),dat))
+  },outs,naNs,SIMPLIFY=FALSE)
+  return(out)
+}
+
+windowWidth<-100
+startCounts<-cacheOperation('work/humanStartCounts.Rdat',mcmapply,function(five,cd,windowWidth=40,...){
   cat('.')
   strand<-strand(cd)@values[1]
   if(any(strand(cd)!=strand)||any(strand(five)!=strand))stop(simpleError('Strand mismatch'))
-  closeFive<-five[c(cumsum(rev(width(five)))[-1],0)<windowWidth]
+  closeFive<-five[c(rev(cumsum(rev(width(five))))[-1],0)<windowWidth]
   closeCd<-cd[c(0,cumsum(width(cd))[-length(cd)])<windowWidth]
   if(strand=='+'){
     newEnd<-start(closeCd)[length(closeCd)]+windowWidth-sum(width(closeCd)[-length(closeCd)])-1
     end(closeCd)[length(closeCd)]<-min(newEnd,end(closeCd)[length(closeCd)])
     newStart<-end(closeFive)[1]-(windowWidth-sum(width(closeFive)[-1]))+1
     start(closeFive)[1]<-max(newStart,start(closeFive)[1])
-    regs<-c(sapply(closeFive,coordToReg),sapply(closeCd,coordToReg))
   }else{
     newEnd<-start(closeFive)[1]+windowWidth-sum(width(closeFive)[-1])-1
     end(closeFive)[1]<-min(newEnd,end(closeFive)[1])
     newStart<-end(closeCd)[length(closeCd)]-(windowWidth-sum(width(closeCd)[-length(closeCd)]))+1
     start(closeCd)[length(closeCd)]<-max(newStart,start(closeCd)[length(closeCd)])
-    regs<-c(rev(sapply(closeCd,coordToReg)),rev(sapply(closeFive,coordToReg)))
   }
-  humanStarts<-do.call(rbind,lapply(sprintf("%s/%s",dataDir,targetFiles),function(x,...)do.call(c,getHuman(x,regs,strand=strand,sizeRange=27:29))))
+  fiveN<-sum(width(closeFive))
+  cdN<-sum(width(closeCd))
+  regs<-list(sapply(closeFive,coordToReg),sapply(closeCd,coordToReg))
+  #if(strand=='+')revFunc<-function(y)lapply(y,function(z)z) else revFunc<-function(y)lapply(y,function(z)rev(z))
+  #humanStarts<-do.call(rbind,lapply(sprintf("%s/%s",dataDir,targetFiles),function(x,revFunc,...)do.call(c,revFunc(getHuman(x,regs,strand=strand,sizeRange=27:29))),revFunc=revFunc))
+  naNs<-c(fiveN-windowWidth,windowWidth-cdN)
+  humanStarts<-do.call(rbind,lapply(sprintf("%s/%s",dataDir,targetFiles),function(x,...)do.call(c,naFillRegs(x,regs,naNs,strand=strand,sizeRange=27:29))))
   rownames(humanStarts)<-names(targetFiles)
   return(humanStarts)
-},fives,cds,SIMPLIFY=FALSE,mc.cores=15,MoreArgs=list(windowWidth=windowWidth))
+},fives,cds,SIMPLIFY=FALSE,mc.cores=18,MoreArgs=list(windowWidth=windowWidth))
 
 
-goodCounts<-startCounts[sapply(startCounts,sum)>50&sapply(startCounts,ncol)==windowWidth*2]
-goodProp<-lapply(goodCounts,function(x)t(apply(x,1,function(y)if(sum(y)>4)y/sum(y) else rep(NA,length(y)))))
+goodCounts<-startCounts[sapply(startCounts,sum,na.rm=TRUE)>50]
+#goodCounts<-startCounts[sapply(startCounts,sum,na.rm=TRUE)>50&sapply(fives,function(x)strand(x)@values[1])=='-']
+goodProp<-lapply(goodCounts,function(x)t(apply(x,1,function(y)if(sum(y,na.rm=TRUE)>4)ifelse(is.na(y),0,y)/sum(y,na.rm=TRUE) else rep(NA,length(y)))))
 library(abind)
 allProps<-do.call(abind,c(goodProp,list(along=3)))
 meanProp<-apply(allProps,c(1,2),mean,na.rm=TRUE)
