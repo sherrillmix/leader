f784b4ee7ea984fa620eb7d2a5ea8b1deb6af4d2
Scott Sherrill-Mix <sherrillmix@users.noreply.github.com>
2016-04-06 16:43:26 -0400

    Working on SVM for TIS

--- learnTIS.R
+++ learnTIS.R
@@ -2,26 +2,74 @@ library(parallel)
 library(e1071)
 if(!exists('features')){
   featureFiles<-list.files('out','data_.*.csv',full.names=TRUE)
-  features<-lapply(featureFiles,read.csv,stringsAsFactors=FALSE,row.names=1)
+  features<-lapply(featureFiles,read.csv,stringsAsFactors=FALSE,row.names=1,check.names=FALSE)
   names(features)<-sub('out/data_([^.]+).csv','\\1',featureFiles)
 }
 
+if(!exists('virus')){
+  virusFiles<-list.files('out','dataVirus_.*.csv',full.names=TRUE)
+  virus<-lapply(virusFiles,read.csv,stringsAsFactors=FALSE,row.names=1,check.names=FALSE)
+  names(virus)<-sub('out/dataVirus_([^.]+).csv','\\1',virusFiles)
+}
+if(any(colnames(virus[[1]][1,])!=colnames(features[[1]][1,])))stop(simpleError('colnames mismatch'))
+
+virus<-lapply(virus,function(x){x[,'ltmChx']<-x[,'LTM_-12']-x[,'CHX_-12'];x})
+features<-lapply(features,function(x){x[,'ltmChx']<-x[,'LTM_-12']-x[,'CHX_-12'];x})
+
+selectColumns<-colnames(virus[[1]])
+selectColumns<-selectColumns[!grepl('total',selectColumns)]
+selectColumns<-selectColumns[grepl('_-12|sum3|ltmChx',selectColumns)]
+
 set.seed(1234)
 trainTest<-lapply(features,function(x){
+  #x[,'LTM_-12']-x[,'CHX_-12']>.03&
+  selector<-x[,'total_Total']>1000&x[,'total_LTM']>100&x[,'total_CHX']>100 &x[,'LTM_-12']>.1 & x[,'CHX_-12']>.1# &x[,'ltmChx']>.03
+  print(summary(selector))
+  x<-x[selector,]
   trainIds<-sample(nrow(x),round(sum(nrow(x)*.5)))
-  list('train'=x[trainIds,],'test'=x[-trainIds,])
+  list('train'=x[trainIds,selectColumns],'test'=x[-trainIds,selectColumns])
 })
 
 svms<-mclapply(names(trainTest),function(datName){
-  message(datName)
+  #message(datName)
   dat<-trainTest[[datName]]
-  model<-svm(dat[['train']],as.factor(rep('TIS',nrow(dat[['train']]))),type='one-classification')
-  print(model)
-  summary(model)
+  message('Training ',datName, ' model')
+  model<-svm(dat[['train']],as.factor(rep('TIS',nrow(dat[['train']]))),type='one-classification',nu=0.1)
+  #print(model)
+  #summary(model)
+  message('Predicting ',datName, ' test set')
   predicts<-predict(model,dat[['test']])
+  message(datName, ' test set:')
   print(summary(predicts))
-  return(list(model,predicts))
+  message('Predicting ',datName, ' viral data')
+  virusPredicts<-predict(model,virus[[datName]][,selectColumns])
+  message(datName, ' viral data:')
+  print(summary(virusPredicts))
+  return(list(model,predicts,virusPredicts))
 },mc.cores=6)
+names(svms)<-names(trainTest)
+humanQuantileCut<-.25
+for(ii in names(svms)){
+  thisNames<-rownames(virus[[ii]])[svms[[ii]][[3]]]
+  #print(thisNames)
+  writeLines(thisNames,sprintf('out/svm_%s.txt',ii))
+  thisNames<-names(svms[[ii]][[2]])[svms[[ii]][[2]]]
+  writeLines(thisNames,sprintf('out/svmHost_%s.txt',ii))
+  pdf(sprintf('out/svm/%s.pdf',ii))
+    xRange<-range(c(virus[[ii]][,'LTM_-12']-virus[[ii]][,'CHX_-12'],trainTest[[ii]][['test']][,'LTM_-12']-trainTest[[ii]][['test']][,'CHX_-12']))
+    yRange<-range(c(virus[[ii]][,'sum3_DMSO']+virus[[ii]][,'sum3_CHX'],trainTest[[ii]][['test']][,'sum3_DMSO']+trainTest[[ii]][['test']][,'sum3_CHX']))
+    plot(virus[[ii]][,'ltmChx'],virus[[ii]][,'sum3_DMSO']+virus[[ii]][,'sum3_CHX'],col=1+svms[[ii]][[3]],xlim=xRange,ylim=yRange)
+    plot(trainTest[[ii]][['test']][,'ltmChx'],trainTest[[ii]][['test']][,'sum3_DMSO']+trainTest[[ii]][['test']][,'sum3_CHX'],col=1+svms[[ii]][[2]],xlim=xRange,ylim=yRange)
+    humanQuant<-quantile(trainTest[[ii]][['test']][,'ltmChx'],humanQuantileCut)
+    virusBreak<-virus[[ii]][,'ltmChx']>humanQuant#&virus[[ii]][,'sum3_DMSO']+virus[[ii]][,'sum3_CHX']>.1
+    plot(virus[[ii]][,'ltmChx'],virus[[ii]][,'sum3_DMSO']+virus[[ii]][,'sum3_CHX'],col=1+virusBreak,xlim=xRange,ylim=yRange)
+    humanBreak<-trainTest[[ii]][['test']][,'ltmChx']>humanQuant #& trainTest[[ii]][['test']][,'sum3_DMSO']+trainTest[[ii]][['test']][,'sum3_CHX']>.1
+    plot(trainTest[[ii]][['test']][,'ltmChx'],trainTest[[ii]][['test']][,'sum3_DMSO']+trainTest[[ii]][['test']][,'sum3_CHX'],col=1+humanBreak,xlim=xRange,ylim=yRange)
+  dev.off()
+  message(ii)
+  thisNames<-rownames(virus[[ii]])[virusBreak]
+  print(thisNames)
+}
 
 if(FALSE){
   ltm1<-do.call(rbind,lapply(goodCounts,function(x)x['LTM1_HIV_CH0236',]))
